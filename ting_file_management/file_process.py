05f0a74b99606ee91ca05c23a60f970b2f3dbd2f
Carlos Vieira <carlepsvieira@gmail.com>
2023-06-14 16:02:36 -0300

    trying to get these errors right

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 from ting_file_management.file_management import txt_importer
@@ -7,7 +6,6 @@ from ting_file_management.file_management import txt_importer
 def process(path_file, instance):
     file_name = path_file
 
-    # Check if the file has already been processed
     for item in instance:
         if item["nome_do_arquivo"] == file_name:
             return
@@ -16,7 +14,7 @@ def process(path_file, instance):
     file_data = {
         "nome_do_arquivo": file_name,
         "qtd_linhas": len(lines),
-        "linhas_do_arquivo": lines
+        "linhas_do_arquivo": lines,
     }
 
     instance.enqueue(file_data)
@@ -29,8 +27,9 @@ def remove(instance):
         sys.stdout.write("Não há elementos\n")
     else:
         removed_file = instance.dequeue()
-        sys.stdout.write(f"Arquivo {removed_file['nome_do_arquivo']} removido com sucesso\n")
-
+        sys.stdout.write(
+            f"Arquivo {removed_file['nome_do_arquivo']} removido com sucesso\n"
+        )
 
 
 def file_metadata(instance, position):
