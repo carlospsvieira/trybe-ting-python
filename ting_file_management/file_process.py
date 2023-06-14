35a60d905109a1d7c26de36a7b154d135277537e
Carlos Vieira <carlepsvieira@gmail.com>
2023-06-14 15:52:36 -0300

    this fixes file process file, I had sent an incomplete version that I had written this morning

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -5,26 +5,37 @@ from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
-    file_name = os.path.basename(path_file)
-    file_data = {}
+    file_name = path_file
 
+    # Check if the file has already been processed
     for item in instance:
         if item["nome_do_arquivo"] == file_name:
             return
 
     lines = txt_importer(path_file)
-    file_data["qtd_linhas"] = len(lines)
-    file_data["linhas_do_arquivo"] = lines
-    file_data["nome_do_arquivo"] = path_file
+    file_data = {
+        "nome_do_arquivo": file_name,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
 
-    instance.append(file_data)
+    instance.enqueue(file_data)
 
     sys.stdout.write(f"{file_data}\n")
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        sys.stdout.write("Não há elementos\n")
+    else:
+        removed_file = instance.dequeue()
+        sys.stdout.write(f"Arquivo {removed_file['nome_do_arquivo']} removido com sucesso\n")
+
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        data = instance.search(position)
+        sys.stdout.write(f"{data}\n")
+    except IndexError:
+        sys.stderr.write("Posição inválida\n")
