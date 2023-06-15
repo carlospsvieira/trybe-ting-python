820554f77c95bb9f830e4df989d42eae3d105508
Carlos Vieira <carlepsvieira@gmail.com>
2023-06-15 10:50:21 -0300

    trying to get theses test right, missing one

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,23 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+    pq.enqueue({"qtd_linhas": 4})
+    pq.enqueue({"qtd_linhas": 7})
+    pq.enqueue({"qtd_linhas": 2})
+    pq.enqueue({"qtd_linhas": 9})
+    pq.enqueue({"qtd_linhas": 5})
+    pq.enqueue({"qtd_linhas": 3})
+
+    assert len(pq) == 6
+
+    values = [2, 3, 4, 5, 7, 9]
+    for value in values:
+        item = pq.dequeue()
+        assert item is not None
+        assert item["qtd_linhas"] == value
+
+    with pytest.raises(IndexError):
+        pq.dequeue()
