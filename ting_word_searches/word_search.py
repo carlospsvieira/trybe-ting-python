e08e8547aea7206f428ebb3e3114c17bdeb28635
Carlos Vieira <carlepsvieira@gmail.com>
2023-06-15 12:33:04 -0300

    exists_word and search_by_word functions implement req 7 and 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,47 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    # Iterate over the files in the queue
+    for file in instance:
+        occurrences = []
+
+        # Iterate over the lines in the file
+        for i, line in enumerate(file["linhas_do_arquivo"], start=1):
+            # Check if the word exists in the line (case insensitive)
+            if word.lower() in line.lower():
+                occurrences.append({"linha": i})
+
+        # If occurrences were found in the file, add the file information to the result
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+    for data in instance:
+        file_name = data.get("nome_do_arquivo")
+        lines = data.get("linhas_do_arquivo")
+        occurrences = []
+        for line_number, line_content in enumerate(lines, 1):
+            if word in line_content.lower():
+                occurrence = {
+                    "conteudo": line_content.strip(),
+                    "linha": line_number,
+                }
+                occurrences.append(occurrence)
+        if occurrences:
+            result = {
+                "arquivo": file_name,
+                "ocorrencias": occurrences,
+                "palavra": word,
+            }
+            results.append(result)
+    return results
